340d263e2d99097a53bcad7ec4bbb1a75a7d6e1b
Rafael Fourquet <fourquet.rafael@gmail.com>
2017-07-11 11:28:19 +0200

    merge/rename some files
    * merge mersennetwister.jl & randomdevice.jl into RNGs.jl
    * delete rand.jl: move docstrings into random.jl and GLOBAL_RNG into srand.jl
      (and delete unused/un-exported globalRNG() function)
    * rename srand.jl into seeding.jl

--- base/random/RNGs.jl
+++ base/random/RNGs.jl
@@ -1,5 +1,49 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+## RandomDevice
+
+const BitIntegerType = Union{map(T->Type{T}, Base.BitInteger_types)...}
+const BoolBitIntegerType = Union{Type{Bool},BitIntegerType}
+const BoolBitIntegerArray = Union{Array{Bool},Base.BitIntegerArray}
+
+if is_windows()
+    struct RandomDevice <: AbstractRNG
+        buffer::Vector{UInt128}
+
+        RandomDevice() = new(Vector{UInt128}(1))
+    end
+
+    function rand(rd::RandomDevice, T::BoolBitIntegerType)
+        rand!(rd, rd.buffer)
+        @inbounds return rd.buffer[1] % T
+    end
+
+    function rand!(rd::RandomDevice, A::BoolBitIntegerArray)
+        ccall((:SystemFunction036, :Advapi32), stdcall, UInt8, (Ptr{Void}, UInt32),
+              A, sizeof(A))
+        A
+    end
+else # !windows
+    struct RandomDevice <: AbstractRNG
+        file::IOStream
+        unlimited::Bool
+
+        RandomDevice(unlimited::Bool=true) =
+            new(open(unlimited ? "/dev/urandom" : "/dev/random"), unlimited)
+    end
+
+    rand(rd::RandomDevice, T::BoolBitIntegerType)   = read( rd.file, T)
+    rand!(rd::RandomDevice, A::BoolBitIntegerArray) = read!(rd.file, A)
+end # os-test
+
+"""
+    RandomDevice()
+
+Create a `RandomDevice` RNG object. Two such objects will always
+generate different streams of random numbers.
+"""
+RandomDevice
+
 ## MersenneTwister
 
 const MTCacheLength = dsfmt_get_min_array_size()
@@ -64,7 +108,7 @@ copy(src::MersenneTwister) =
     r1.idx == r2.idx
 
 
-## Low level API for MersenneTwister
+### Low level API
 
 @inline mt_avail(r::MersenneTwister) = MTCacheLength - r.idx
 @inline mt_empty(r::MersenneTwister) = r.idx == MTCacheLength
@@ -97,10 +141,11 @@ end
 rand_ui10_raw(r::MersenneTwister) = rand_ui52_raw(r)
 rand_ui23_raw(r::MersenneTwister) = rand_ui52_raw(r)
 
-## MersenneTwister jump
+### randjump
 
 """
-    randjump(r::MersenneTwister, jumps::Integer, [jumppoly::AbstractString=dSFMT.JPOLY1e21]) -> Vector{MersenneTwister}
+    randjump(r::MersenneTwister, jumps::Integer,
+             [jumppoly::AbstractString=dSFMT.JPOLY1e21]) -> Vector{MersenneTwister}
 
 Create an array of the size `jumps` of initialized `MersenneTwister` RNG objects. The
 first RNG object given as a parameter and following `MersenneTwister` RNGs in the array are
